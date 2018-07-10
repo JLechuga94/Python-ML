e75cdbf227a8eab7816c40e04a8ff53f89f479f5
Julian Lechuga <julian.lechuga305@gmail.com>
2018-07-10 16:45:09 +0900

    Quantile calculation upper and lower limit for IQR Interquartilic range

--- boxplot.py
+++ boxplot.py
@@ -4,11 +4,16 @@ import numpy as np
 data = pandas.read_csv("../datasets/customer-churn-model/Customer Churn Model.txt")
 # print(data.head())
 
-# Sturges law to adequate number of divisions for size of sample
-k = int(np.ceil(1 + np.log2(3333)))
-plt.figure(2)
-plt.hist(data["Day Calls"], bins=k)
-plt.xlabel("Nùmero de llamadas al día")
-plt.ylabel("Frecuencia")
-plt.title("Histograma del número de llamadas por día")
+IQR = data["Day Calls"].quantile(0.75)-data["Day Calls"].quantile(0.25)
+lower_boxplot_limit = data["Day Calls"].quantile(0.25) - 1.5*IQR
+upper_boxplot_limit = data["Day Calls"].quantile(0.75) + 1.5*IQR
+
+print(data["Day Calls"].describe())
+print(IQR)
+print(lower_boxplot_limit)
+print(upper_boxplot_limipr)
+
+plt.boxplot(data["Day Calls"])
+plt.ylabel("Número de llamadas diarias")
+plt.title("Boxplot del número de llamadas por día")
 plt.show()
