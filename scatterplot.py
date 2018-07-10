f0be0667c2f37920ef4ce6fa5240926a9ffea5a2
Julian Lechuga <julian.lechuga305@gmail.com>
2018-07-10 16:16:31 +0900

    Scatter plot script with subplots

---
+++ scatterplot.py
@@ -0,0 +1,11 @@
+import pandas
+import matplotlib.pyplot as plt
+data = pandas.read_csv("../datasets/customer-churn-model/Customer Churn Model.txt")
+# print(data.head())
+
+figure, axs = plt.subplots(2,2, sharey=True, sharex=True)
+data.plot(kind="scatter", x="Day Mins", y="Day Charge", ax=axs[0,0])
+data.plot(kind="scatter", x="Night Mins", y="Night Charge", ax=axs[0,1])
+data.plot(kind="scatter", x="Day Calls", y="Day Charge", ax=axs[1,0])
+data.plot(kind="scatter", x="Night Calls", y="Night Charge", ax=axs[1,1])
+plt.show()
