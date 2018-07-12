629fe574bdcde746a477f8ecd11411c7b649e2b4
Julian Lechuga <julian.lechuga305@gmail.com>
2018-07-12 17:12:06 +0900

    Added new directory for datasets division in training and testing, added script for dividing a dataset based on a normal distribution

---
+++ data_division/normaldistribution_division.py
@@ -0,0 +1,28 @@
+# Divition of dataset for training and testing
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+
+division_percentage = 0.75
+data = pandas.read_csv("../../datasets/customer-churn-model/Customer Churn Model.txt")
+# print(data.head())
+
+# Division using normal distribution
+
+a = np.random.randn(len(data))
+# plt.hist(a)
+
+check = (a < division_percentage)
+check2 = (a > division_percentage)
+
+plt.hist(check)
+
+training_dataset = data[check]
+testing_dataset = data[check2]
+
+# Amount of elements for each division of the DS
+print(len(training_dataset))
+print(len(testing_dataset))
+
+
+plt.show()
