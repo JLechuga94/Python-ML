433bb96887762c66882a5471d9298690350c0691
Julian Lechuga <julian.lechuga305@gmail.com>
2018-07-19 17:11:56 +0900

    Added implementation of pandas function for pearson coefficient

--- statistics_basics/variable_correlation.py
+++ statistics_basics/variable_correlation.py
@@ -7,6 +7,8 @@ data_ads = pandas.read_csv("../../datasets/ads/Advertising.csv")
 print(data_ads.head())
 data_columns = data_ads.columns.values
 
+# Manual calculation of Pearson correlation
+
 
 def pearson_coeff(df, var1, var2):
     corrn = (data_ads[var1] - np.mean(data_ads[var1])) * (data_ads[var2] - np.mean(data_ads[var2]))
@@ -21,5 +23,9 @@ for x in data_columns:
         if x != y:
             print(x + ", " + y + ": " + str(pearson_coeff(data_ads, x, y)))
 
-plt.plot(data_ads["TV"], data_ads["Sales"], "o")
+# Pandas function for calculation of Pearson correlation
+# Outputs a simetric matrix of correlation
+print(data_ads.corr())
+plt.matshow(data_ads.corr())  # Graphic representation of the corr matrix
+# plt.plot(data_ads["TV"], data_ads["Sales"], "o")
 plt.show()
