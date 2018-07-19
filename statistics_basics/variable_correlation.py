0a7ff411bcf9f0e6047eb979e8dbc4cc91d04f93
Julian Lechuga <julian.lechuga305@gmail.com>
2018-07-19 16:41:58 +0900

    Correlation between variables for a dataset involving expenses on marketing through different channels related to sales

---
+++ statistics_basics/variable_correlation.py
@@ -0,0 +1,25 @@
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data_ads = pandas.read_csv("../../datasets/ads/Advertising.csv")
+print(data_ads.head())
+data_columns = data_ads.columns.values
+
+
+def pearson_coeff(df, var1, var2):
+    corrn = (data_ads[var1] - np.mean(data_ads[var1])) * (data_ads[var2] - np.mean(data_ads[var2]))
+    corr1 = (data_ads[var1] - np.mean(data_ads[var1])) ** 2
+    corrn2 = (data_ads[var2] - np.mean(data_ads[var2])) ** 2
+    pearson = sum(corrn)/np.sqrt(sum(corr1) * sum(corrn2))
+    return pearson
+
+
+for x in data_columns:
+    for y in data_columns:
+        if x != y:
+            print(x + ", " + y + ": " + str(pearson_coeff(data_ads, x, y)))
+
+plt.plot(data_ads["TV"], data_ads["Sales"], "o")
+plt.show()
